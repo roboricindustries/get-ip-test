1aedd946b28ae0d02b08f7384c0b0f3c3a807a6a
Ilya <ilya.makarochkin@yandex.ru>
2025-06-10 12:36:20 +0500

    logger and limiter

--- api/go.mod
+++ api/go.mod
@@ -2,7 +2,11 @@ module app
 
 go 1.23.5
 
-require github.com/gin-gonic/gin v1.10.1
+require (
+	github.com/gin-gonic/gin v1.10.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/ulule/limiter/v3 v3.11.2
+)
 
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
@@ -22,6 +26,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
--- api/src/main.go
+++ api/src/main.go
@@ -2,9 +2,56 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/ulule/limiter/v3"
+	ginlimiter "github.com/ulule/limiter/v3/drivers/middleware/gin"
+	"github.com/ulule/limiter/v3/drivers/store/memory"
 )
+
+func InitializeLogger() *logrus.Logger {
+	logFile, err := os.OpenFile("gin.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		logrus.Fatalf("Failed to create log file: %v", err)
+	}
+
+	logger := logrus.New()
+	logger.SetOutput(logFile)
+	logger.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: time.RFC3339,
+	})
+	logger.SetLevel(logrus.InfoLevel)
+
+	return logger
+}
+
+func LoggerMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		duration := time.Since(start)
+		if l, exists := c.Get("logger"); exists {
+			log := l.(*logrus.Logger)
+			log.WithFields(logrus.Fields{
+				"latency":   duration.Seconds(),
+				"client_ip": c.ClientIP(),
+				"method":    c.Request.Method,
+				"path":      c.Request.URL.Path,
+				"status":    c.Writer.Status(),
+			}).Info("[GIN] request handled")
+		}
+	}
+}
+
+func InjectLogger(logger *logrus.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("logger", logger)
+		c.Next()
+	}
+}
 
 func GetIp(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
@@ -17,16 +64,37 @@ func GetIp(c *gin.Context) {
 func SetupRoutes(r *gin.Engine, path string) {
 	api := r.Group(path)
 
+	rate, err := limiter.NewRateFromFormatted("5-M")
+	if err != nil {
+		panic(err)
+	}
+
+	// In-memory store (for production, use Redis)
+	store := memory.NewStore()
+
+	// Create limiter instance
+	limiterInstance := limiter.New(
+		store, rate,
+		limiter.WithClientIPHeader("X-Real-IP"),
+	)
+
+	// Apply middleware
+	rateMiddleware := ginlimiter.NewMiddleware(limiterInstance)
+
 	ipRoutes := api.Group(
 		"/ip",
 	)
+	ipRoutes.Use(rateMiddleware)
 	{
 		ipRoutes.GET("/", GetIp)
 	}
 }
 
 func main() {
+	logger := InitializeLogger()
 	r := gin.Default()
+	r.Use(InjectLogger(logger))
+	r.Use(LoggerMiddleware())
 	r.TrustedPlatform = "X-Real-IP"
 	SetupRoutes(r, "api")
 	r.Run("0.0.0.0:" + "8080")
