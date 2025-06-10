8c69cc7e5d09ec38620db74dcdd36ae050f90486
Ilya <ilya.makarochkin@yandex.ru>
2025-06-10 12:10:56 +0500

    setup

---
+++ api/src/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetIp(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"client_ip":       c.ClientIP(),
+		"x-real-ip":       c.Request.Header.Get("X-Real-IP"),
+		"x-forwarded-for": c.Request.Header.Get("X-Forwarded-For"),
+	})
+}
+
+func SetupRoutes(r *gin.Engine, path string) {
+	api := r.Group(path)
+
+	ipRoutes := api.Group(
+		"/ip",
+	)
+	{
+		ipRoutes.GET("/", GetIp)
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.TrustedPlatform = "X-Real-IP"
+	r.Run("0.0.0.0:" + "8080")
+}
